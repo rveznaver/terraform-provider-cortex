82af806881d036a56bdc1737ced0287fe6c5cc49
Robert Vežnaver <rveznaver@users.noreply.github.com>
2026-02-12 22:55:17 -0500

    mark alertmanager configuration as sensitive

--- internal/cortex/resource_alertmanager.go
+++ internal/cortex/resource_alertmanager.go
@@ -27,6 +27,7 @@ func resourceAlertmanager() *schema.Resource {
 				Type:        schema.TypeString,
 				Description: "Content of the Alertmanager configuration.",
 				Required:    true,
+				Sensitive:   true,
 				DiffSuppressFunc: func(k, old, new string, d *schema.ResourceData) bool {
 					olds, err := formatYAML(old)
 					if err != nil {
